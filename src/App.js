a12762c5016c97f8df0a24c8073063e1ac69ea1d
FatihMandirali <fatih.mandirali@hotmail.com>
2020-04-18 21:37:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserPage from './components/pages/UserPage'
+import NewUserPage from './components/pages/NewUserPage'
+import {Route} from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
+import {
+  Container,
+} from 'semantic-ui-react';
 
+import TopMenu from './components/TopMenu'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {/* <Link to="userlist">UserList</Link>
+     <Route path='/userlist' component={UserPage}></Route> */}
+    <TopMenu></TopMenu>
+    <Container text style={{ marginTop: '7em' }}>
+     <Route exact path='/' component={UserPage} ></Route>
+     <Route exact path='/yeni' component={NewUserPage} ></Route>
+     <Route exact path='/detay/:id' component={NewUserPage} ></Route>
+     
+    </Container>
+    <Footer></Footer>
+   
     </div>
   );
 }
